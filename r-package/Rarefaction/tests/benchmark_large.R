e79d4435321df1f38e5940aca27349fece1ede28
openpaul <github@paulsaary.de>
2016-05-31 10:32:47 +0200

    change test of large matrix

--- r-package/Rarefaction/tests/benchmark_large.R
+++ r-package/Rarefaction/tests/benchmark_large.R
@@ -1,16 +1,16 @@
 
-path <- '/Users/saary/projekt/testData/mh14/mh.14.gene.profile.screened.hg19.on.1000RefGeneCat.1-2.fastx.allbest.l30.p95.base.only.unique.raw.gene'
+path <- '/home/saary/rareFalk/testData/OTU.txt'
 
 
 print("Rarefaction with path, without returning the values")
 require("rarefaction")
 ptm <- proc.time()
-result.rarefy   <- rarefaction::rare(input = path, 
-                                     output = "", 
+result.rarefy   <- rarefaction::rare(input = path,
+                                     output = "",
                                      rareDepth = 1000,
-                                     repeats = 10, 
-                                     NoOfMatrices = 1, 
-                                     returnObject = F, 
+                                     repeats = 10,
+                                     NoOfMatrices = 1,
+                                     returnObject = F,
                                      verbose = F)
 proc.time() - ptm
 str(result.rarefy)
@@ -20,16 +20,16 @@ rm(list = ls())
 
 
 print("Rarefaction with path, while returning the values")
-path <- '/Users/saary/projekt/testData/mh14/mh.14.gene.profile.screened.hg19.on.1000RefGeneCat.1-2.fastx.allbest.l30.p95.base.only.unique.raw.gene'
+path <- '/home/saary/rareFalk/testData/OTU.txt'
 
 require("rarefaction")
 ptm <- proc.time()
-result.rarefy   <- rarefaction::rare(input = path, 
-                                     output = "", 
+result.rarefy   <- rarefaction::rare(input = path,
+                                     output = "",
                                      rareDepth = 1000,
-                                     repeats = 10, 
-                                     NoOfMatrices = 1, 
-                                     returnObject = T, 
+                                     repeats = 10,
+                                     NoOfMatrices = 1,
+                                     returnObject = T,
                                      verbose = F)
 proc.time() - ptm
 #str(result.rarefy)
@@ -37,7 +37,7 @@ rm(list = ls())
 
 
 print("vegan")
-path <- '/Users/saary/projekt/testData/mh14/mh.14.gene.profile.screened.hg19.on.1000RefGeneCat.1-2.fastx.allbest.l30.p95.base.only.unique.raw.gene'
+path <- '/home/saary/rareFalk/testData/OTU.txt'
 require("vegan")
 ptm <- proc.time()
 data            <- read.table(file = path, header = TRUE, row.names = 1)
@@ -45,9 +45,3 @@ samplesize      <- min(rowSums(data))
 result.vegan    <- vegan::rrarefy(x = data, sample = samplesize)
 proc.time() - ptm
 rm(list = ls())
-
-
-
-
-
-
