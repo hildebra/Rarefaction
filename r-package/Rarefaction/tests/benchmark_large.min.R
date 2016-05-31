adc85de04901a942f64269ed73836e88b93ebb0a
openpaul <github@paulsaary.de>
2016-05-31 11:16:26 +0200

    update test

--- r-package/Rarefaction/tests/benchmark_large.min.R
+++ r-package/Rarefaction/tests/benchmark_large.min.R
@@ -1,7 +1,7 @@
 path <- '/home/saary/data/testdata.csv'
 
 
-print("Rarefaction with path, without returning the values")
+print("Rarefaction with path, without matrix transformation")
 require("rarefaction")
 ptm <- proc.time()
 result.rarefy   <- rarefaction::rare(input = path,
@@ -10,8 +10,27 @@ result.rarefy   <- rarefaction::rare(input = path,
                                      repeats = 10,
                                      NoOfMatrices = 1,
                                      returnObject = F,
-                                     verbose = F,
+                                     verbose = T,
 									 margin = 1)
+proc.time() - ptm
+str(result.rarefy)
+rm(list = ls())
+
+
+path <- '/home/saary/data/testdata.csv'
+
+
+print("Rarefaction with path, with tranforming")
+require("rarefaction")
+ptm <- proc.time()
+result.rarefy   <- rarefaction::rare(input = path,
+                                     output = "",
+                                     rareDepth = 1000,
+                                     repeats = 10,
+                                     NoOfMatrices = 1,
+                                     returnObject = F,
+                                     verbose = T,
+									 margin = 2)
 proc.time() - ptm
 str(result.rarefy)
 rm(list = ls())
