6b8b501f6b824a0fffd70bddcd775ad3f9f0ae91
openpaul <github@paulsaary.de>
2016-07-29 15:08:37 +0200

    code structure of main R function nice

--- r-package/rtk/R/rarefaction.R
+++ r-package/rtk/R/rarefaction.R
@@ -11,70 +11,66 @@ rare.status <- function(msg, verbose=TRUE){
 rtk <- function(input, repeats = 10, depth = 0, ReturnMatrix = 0, margin = 2, verbose = FALSE, threads = 1, tmpdir = NULL ){
 
 
-	if(repeats < ReturnMatrix ){
-		repeats <- ReturnMatrix
-		warning(paste("Repeats can not be smaller than number of matrices to return. Repeats set to match ReturnMatrix. repeats = ReturnMatrix =", repeats, sep=" "))
-	}
-
-	#if(!all(depth > 0)){
-	#	warning("Will now rarefy to 0.95 times the smallest column sum")
-	#}
-
-	# sort depths
-	depth <- sort(as.numeric(depth))
+  if(repeats < ReturnMatrix ){
+    repeats <- ReturnMatrix
+    warning(paste("Repeats can not be smaller than number of matrices to return. Repeats set to match ReturnMatrix. repeats = ReturnMatrix =", repeats, sep=" "))
+  }
 
 
-	# convert dataframes
-	if(class(input) == "data.frame"){
-		input <- as.matrix(input)
-	}
+  # sort depths
+  depth <- sort(as.numeric(depth))
 
-	#validate if input is a path or a matrix
-	if(class(input) == "matrix"){
-		rare.status("Matrix object supplied for analysis", verbose)
-		# validate that the matrix is numeric
-		if(!is.numeric(input)){
-			stop("The supplied matrix object is not numeric. Please check your input matrix.")
-		}
+  # convert dataframes
+  if(class(input) == "data.frame"){
+    input <- as.matrix(input)
+  }
 
-		# pass 1:x to Cpp as colnames
-		removeCnames <- FALSE
-		removeRnames <- FALSE
-		if(is.null(colnames(input))){
-			colnames(input) <- paste("col ", seq(1:ncol(input)), sep="")
-			removeCnames <- TRUE
-		}
-		if(is.null(rownames(input))){
-			rownames(input) <- paste("row ", seq(1:nrow(input)), sep="")
-			removeRnames <- TRUE
-		}
+  #validate if input is a path or a matrix
+  if(class(input) == "matrix"){
+    rare.status("Matrix object supplied for analysis", verbose)
+    # validate that the matrix is numeric
+    if(!is.numeric(input)){
+      stop("The supplied matrix object is not numeric. Please check your input matrix.")
+    }
 
-		# call the actual software
+    # pass 1:x to Cpp as colnames
+    removeCnames <- FALSE
+    removeRnames <- FALSE
+    if(is.null(colnames(input))){
+      colnames(input) <- paste("col ", seq(1:ncol(input)), sep="")
+      removeCnames <- TRUE
+    }
+    if(is.null(rownames(input))){
+      rownames(input) <- paste("row ", seq(1:nrow(input)), sep="")
+      removeRnames <- TRUE
+    }
 
-		result <- lapply(depth, function(d){
-						res <- rcpp_rarefaction("", input, colnames(input),
-								rownames(input), repeats, d,
-								ReturnMatrix, verbose, threads,
-								margin, "NULL", FALSE)
+    # call the actual software
 
-						# remove col and/or row names, as we've added them for
-						# the Cpp software to work well
-						if(removeRnames == TRUE && removeRnames == TRUE){
-							res$raremat <- lapply(res$raremat, unname)
-						}else{
-							if(removeRnames == TRUE){
-								res$raremat <- lapply(res$raremat, function(x) {rownames(x) <- NULL; return (x)})
-							}
-							if(removeCnames == TRUE){
-								res$raremat <- lapply(res$raremat, function(x) {colnames(x) <- NULL; return (x)})
-							}
-						}
-						return(res)
-					})
+    result <- lapply(depth, function(d){
+            res <- rcpp_rarefaction("", input, colnames(input),
+                                    rownames(input), repeats, d,
+                                    ReturnMatrix, verbose, threads,
+                                    margin, "NULL", FALSE)
+
+            # remove col and/or row names, as we've added them for
+            # the Cpp software to work well
+            if(removeRnames == TRUE && removeRnames == TRUE){
+              res$raremat <- lapply(res$raremat, unname)
+            }else{
+              if(removeRnames == TRUE){
+                res$raremat <- lapply(res$raremat, function(x) {rownames(x) <- NULL; return (x)})
+              }
+              if(removeCnames == TRUE){
+                res$raremat <- lapply(res$raremat, function(x) {colnames(x) <- NULL; return (x)})
+              }
+            }
+            return(res)
+          })
 
 
-	}else if(class(input) == "character"){
-	  rare.status("A path to a matrix file was supplied", verbose)
+  }else if(class(input) == "character"){
+    rare.status("A path to a matrix file was supplied", verbose)
     if(!is.null(tmpdir) & margin == 2){
       uselowmem <- TRUE;
       rare.status("Low memory mode will be used. Temporary files will be stored on storage medium", verbose)
@@ -85,49 +81,48 @@ rtk <- function(input, repeats = 10, depth = 0, ReturnMatrix = 0, margin = 2, ve
       tmpdir     <- "NULL";
     }
 
-		# validate that the file exists
-		if(!file.exists(input)){
-			stop(paste("The file can not be found. Please verify that the file exists in the given location. The path given is:", input, sep = " "))
-		}
-		result <- lapply(depth, function(d){
-							res <- rcpp_rarefaction( input,
-	                        matrix(1,1,c(1)),
-							c(NA),c(NA), # col and rownames
-	                        repeats, d,
-							ReturnMatrix,
-	                        verbose, threads,
-							margin, tmpdir, uselowmem)
-							return(res)
-						})
+    # validate that the file exists
+    if(!file.exists(input)){
+      stop(paste("The file can not be found. Please verify that the file exists in the given location. The path given is:", input, sep = " "))
+    }
+    result <- lapply(depth, function(d){
+              res <- rcpp_rarefaction( input,
+                                       matrix(1,1,c(1)),
+                                       c(NA),c(NA), # col and rownames
+                                       repeats, d,
+                                       ReturnMatrix,
+                                       verbose, threads,
+                                       margin, tmpdir, uselowmem)
+              return(res)
+            })
   }else{
     stop("Unknown input type. Path to a file (character) or a numeric matrix are accepted types.")
   }
-  	result <- lapply(result, function(res){
+    result <- lapply(result, function(res){
 
-		if(length(res$skipped) > 0){
-			warning(paste(length(res$skipped), "samples where skipped because the depth was greater than the number of elements in the sample."))
-		}
+    if(length(res$skipped) > 0){
+      warning(paste(length(res$skipped), "samples where skipped because the depth was greater than the number of elements in the sample."))
+    }
 
-	  # calculate median for diversity measures
-	  measures 					<- c('richness', 'shannon', 'simpson', 'invsimpson', 'chao1', 'eveness')
-	  res$div.median 			<- lapply(measures, r.median, x=res$divvs)
-	  names(res$div.median) 	<- paste("median.", measures, sep = "")
+    # calculate median for diversity measures
+    measures               <- c('richness', 'shannon', 'simpson', 'invsimpson', 'chao1', 'eveness')
+    res$div.median         <- lapply(measures, r.median, x=res$divvs)
+    names(res$div.median)  <- paste("median.", measures, sep = "")
 
-	  return(res)
-	})
+    return(res)
+  })
 
-	if(length(depth) == 1){
-		result <- result[[1]]
-	}else{
+  if(length(depth) == 1){
+    result <- result[[1]]
+  }else{
     names(result) <- depth
   }
 
-	result$depths <- depth
-	result$repeats <- repeats
-	# set our class
-	class(result) <- "rtk";
-	return(result)
+  result$depths    <- depth
+  result$repeats   <- repeats
+  class(result)    <- "rtk";
 
+  return(result)
 }
 
 
@@ -146,14 +141,14 @@ get.diversity <- function(obj, div = 'richness', multi = FALSE){
         obj <- obj[[1]]
       }
       y <- sapply(obj$divvs, function(x){
-        r <- x[[div]]
-        return(r)
+          r <- x[[div]]
+          return(r)
         })
     }else{
       y <- lapply(obj$depths, function(i){
-        ia <- as.character(i)
-        x <- get.diversity(obj[ia], div, multi = TRUE)
-        return(x)
+          ia <- as.character(i)
+          x <- get.diversity(obj[ia], div, multi = TRUE)
+          return(x)
         })
     }
     return(y)
