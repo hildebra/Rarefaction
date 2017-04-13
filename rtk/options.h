5d840020623534b68dd2a68a72468378a545f97f
Paul <github@paulsaary.de>
2017-04-13 11:11:45 +0200

    change options init to accomodate old R versions

--- rtk/options.h
+++ rtk/options.h
@@ -4,33 +4,33 @@
 struct options
 {
 public:
-	options(int argc, char** argv);
-	options(std::string, std::string , int repeats, std::vector<long> depth, int NoOfMatrices, bool verbose, unsigned int threads);
-	void print_rare_details();
-	//~options();
+    options(int argc, char** argv);
+    options(std::string, std::string , int repeats, std::vector<long> depth, int NoOfMatrices, bool verbose, unsigned int threads);
+    void print_rare_details();
+    //~options();
 
-	//vars
-  std::string input = "";
-  std::string output = "";
-  std::string mode  = "";
-  std::string referenceDir = "";
-  std::string referenceFile = "";
-  std::string map = "";
-  std::vector<long> depth;
-  long depthMin;
-  unsigned int repeats = 10;
-  unsigned int write = 0;
-  unsigned int threads = 1;
-  bool writeSwap = true;
-  bool verbose = false;
+    //vars
+    std::string input;
+    std::string output;
+    std::string mode;
+    std::string referenceDir;
+    std::string referenceFile;
+    std::string map;
+    std::vector<long> depth;
+    long depthMin;
+    unsigned int repeats ;
+    unsigned int write ;
+    unsigned int threads ;
+    bool writeSwap ;
+    bool verbose;
 
-  std::string modDB;
-  int modRedund;
-  float modEnzCompl;
-  float modModCompl;
-  bool modWrXtraInfo;
-  bool modCollapse;
-  bool calcCoverage;
+    std::string modDB;
+    int modRedund;
+    float modEnzCompl;
+    float modModCompl;
+    bool modWrXtraInfo;
+    bool modCollapse;
+    bool calcCoverage;
 
-  std::string xtra;
+    std::string xtra;
 };
