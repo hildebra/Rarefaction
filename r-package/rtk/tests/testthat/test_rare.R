2284d2aac22800d89200abe08514d61c184c0a6a
Paul <github@paulsaary.de>
2017-07-26 16:19:15 +0200

    add unit test for skipped samples

--- r-package/rtk/tests/testthat/test_rare.R
+++ r-package/rtk/tests/testthat/test_rare.R
@@ -39,3 +39,12 @@ test_that("zeros are reproduced", {
   expect_equal(nullpos,nullpos.after)
   expect_equal(data.r,data)
 })
+
+test_that("Skipped samples are reported", {
+    # columns that contain only zeros or are saller than depth
+    # are skipped. this should be mentioned
+    data <- matrix(c(0,10,20,30,0, 0,0,0,2,3, 10,20,0,0,30, 10,20,30,0,0, 10,0,20,0,30, 10, 0,0,20,30), 5)
+    data.r <- rtk(data, 1, 10)
+    expect_that(length(data.r$skipped),equals(1)) 
+
+}
